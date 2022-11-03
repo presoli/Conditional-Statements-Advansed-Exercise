02295e0ab93f2428edbfc622c0fa597e7cc8e24e
presoli <preslav2103@gmail.com>
2022-11-03 19:00:42 +0200

    Add project files.

---
+++ 01. Cinema/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Conditional_Statements_Advanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                      
+        }
+    }
+}
